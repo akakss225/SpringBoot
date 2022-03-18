afb0f5c92de690a18008eb686b71f4c6c31e00e3
akakss225 <akakss225@naver.com>
2022-03-18 17:43:26 +0900

    Layout 분리하기 >> 템플릿 만들어보기

--- Helloworld/src/main/java/com/example/helloworld/controller/FirstController.java
+++ Helloworld/src/main/java/com/example/helloworld/controller/FirstController.java
@@ -12,4 +12,11 @@ public class FirstController {
         model.addAttribute("username", "Sumin");
         return "greetings";
     }
+
+    @GetMapping("/bye")
+    public String seeYouNext(Model model){
+        model.addAttribute("username", "Sumin");
+        return "goodbye";
+    }
+
 }
